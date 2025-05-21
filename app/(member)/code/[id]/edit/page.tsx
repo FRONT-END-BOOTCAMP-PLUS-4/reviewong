9547515038bbefcf84ff50ce7bc3c990fc12e942
devGO <been2zz@naver.com>
2025-05-21 17:30:42 +0900

    feat: CodeEditPage > CodeSnippetEditContainer 추가

--- app/(member)/code/[id]/edit/page.tsx
+++ app/(member)/code/[id]/edit/page.tsx
@@ -1,3 +1,10 @@
-export default function CodeEditPage() {
-  return <div>page</div>;
+import CodeSnippetEditContainer from './CodeSnippetEditContainer';
+
+export default function CodeEditPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <CodeSnippetEditContainer id={id} />
+    </main>
+  );
 }
