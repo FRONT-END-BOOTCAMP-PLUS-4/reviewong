fa092a4cb460aa444aef900ed23d67e0a760c7ca
devGO <been2zz@naver.com>
2025-05-21 12:11:42 +0900

    feat: CodeSnippetDetailPage > CodeSnippetDetailContainer

--- app/code/[id]/page.tsx
+++ app/code/[id]/page.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import CodeSnippetDetailContainer from './CodeSnippetDetailContainer';
 
-export default function CodeDetailPage() {
-  return <div>page</div>;
+export default function CodeSnippetDetailPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+
+  return (
+    <main>
+      <CodeSnippetDetailContainer id={id} />
+      {/* <ReviewList snippetId={id} /> */}
+    </main>
+  );
 }
