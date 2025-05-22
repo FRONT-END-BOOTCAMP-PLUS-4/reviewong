631a7cbb6c777b923f1052d6f2bf4a2548ec932e
1juyoung <myjuyoung2@gmail.com>
2025-05-22 16:16:11 +0900

    feat: 코드 상세 페이지에 리뷰 리스트 연동 #12

--- app/code/[id]/page.tsx
+++ app/code/[id]/page.tsx
@@ -1,12 +1,14 @@
+import ReviewListContainer from '@/app/review/containers/ReviewListContainer';
 import CodeSnippetDetailContainer from './CodeSnippetDetailContainer';
 
 export default function CodeSnippetDetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
+  const codeId = parseInt(id, 10);
 
   return (
     <main>
       <CodeSnippetDetailContainer id={id} />
-      {/* <ReviewList snippetId={id} /> */}
+      <ReviewListContainer codeId={codeId} />
     </main>
   );
 }
