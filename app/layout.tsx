07bd2d3f90b51713cb34624b265a8c44a54d1f8d
devGO <been2zz@naver.com>
2025-05-22 11:42:10 +0900

    feat: Header component 생성 및 layout 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import Header from './components/Header';
 import QueryProvider from './components/QueryProvider';
 import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <QueryProvider>{children}</QueryProvider>
+        <QueryProvider>
+          <Header />
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
