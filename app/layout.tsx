11e962910524ccd7c6167e07e81ea700456dfe6b
DKULena <32173057@dankook.ac.kr>
2025-05-23 11:30:38 +0900

    feat: 세션 프로바이더 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
-import Header from './components/Header';
 import QueryProvider from './components/QueryProvider';
+import Provider from './components/Provider';
+import Header from './components/Header';
 import './globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
         <QueryProvider>
-          <Header />
-          {children}
+          <Provider>
+            <Header />
+            {children}
+          </Provider>
         </QueryProvider>
       </body>
     </html>
