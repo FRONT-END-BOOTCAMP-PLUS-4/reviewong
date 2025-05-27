e5d3b562a52eefeed73b76241fa5bb84fdd9e703
devGO <been2zz@naver.com>
2025-05-27 11:14:04 +0900

    feat: mypage > heatmap (임시) #32

--- app/(member)/my/page.tsx
+++ app/(member)/my/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+// @ts-ignore
+import CalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+
 export default function Mypage() {
-  return <div>page</div>;
+  const [heatmapData, setHeatmapData] = useState<{ date: string; count: number }[]>([]);
+
+  useEffect(() => {
+    const fetchHeatmapData = async () => {
+      try {
+        const res = await fetch('/api/member/my/review_counts');
+        if (!res.ok) {
+          throw new Error('Failed to fetch heatmap data');
+        }
+        const data = await res.json();
+        setHeatmapData(data.reviewHeatmap);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchHeatmapData();
+  }, []);
+
+  return (
+    <div>
+      <CalendarHeatmap
+        startDate={new Date('2025-01-01')}
+        endDate={new Date('2025-12-31')}
+        values={heatmapData}
+      />
+    </div>
+  );
 }
