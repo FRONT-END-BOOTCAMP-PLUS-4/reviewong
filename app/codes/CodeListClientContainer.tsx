74f167413d6c2cb31f6be8620da3cdb341cb176e
1juyoung <myjuyoung2@gmail.com>
2025-05-28 14:17:32 +0900

    feat: 코드 리스트 csr fetch #35 - 카테고리선택드롭다운박스 컴포넌트 사용 - 카테고리 선택시 csr로 fetch 되도록 구현

---
+++ app/codes/CodeListClientContainer.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { notFound } from 'next/navigation';
+import CodeList, { Code } from './CodeList';
+import { CategorySelectDropdown } from '../(member)/codes/create/components/CategorySelectDropdown';
+
+interface Props {
+  codes: Code[];
+}
+
+export default function CodeListClientContainer({ codes }: Props) {
+  const [categoryCodes, setCategoryCodes] = useState<Code[]>(codes);
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+
+  const fetchCodesByCategories = async (categoriesId: number[]) => {
+    try {
+      const query = categoriesId.join(',');
+      const res = await fetch(`/api/codes?categories=${query}`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch codes');
+      }
+      const result = await res.json();
+      const data = result.map((item: any) => item.data);
+      setCategoryCodes(data);
+    } catch (error) {
+      console.error(error);
+      notFound();
+    }
+  };
+
+  useEffect(() => {
+    fetchCodesByCategories(selectedCategories);
+  }, [selectedCategories]);
+
+  return (
+    <>
+      <div className="flex flex-row m-5 items-center gap-3">
+        <h3 className="font-bold">코드 목록</h3>
+        <CategorySelectDropdown selected={selectedCategories} onChange={setSelectedCategories} />
+      </div>
+      <CodeList codes={categoryCodes} />
+    </>
+  );
+}
