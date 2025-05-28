59669f36f7beef8abe258a8c808b27c1a425eac5
DKULena <32173057@dankook.ac.kr>
2025-05-28 11:17:29 +0900

    remove: comment 삭제

--- app/reviews/containers/ReviewListContainer.tsx
+++ app/reviews/containers/ReviewListContainer.tsx
@@ -22,35 +22,3 @@ export default async function ReviewListContainer({ codeId }: { codeId: number }
     </>
   );
 }
-// 'use client';
-
-// import { useEffect, useState } from 'react';
-// import { ReviewView } from '@/domain/entities/ReviewView';
-// import ReviewListClientContainer from './ReviewListClientContainer';
-
-// export default function ReviewListContainer({ codeId }: { codeId: number }) {
-//   const [reviews, setReviews] = useState<ReviewView[]>([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/reviews`, {
-//       cache: 'no-store',
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setReviews(data);
-//         setLoading(false);
-//       });
-//   }, [codeId]);
-
-//   if (loading) {
-//     return <div>리뷰 로딩 중...</div>;
-//   }
-
-//   return (
-//     <>
-//       <h3 className="font-bold m-5">리뷰 목록 ({reviews.length})</h3>
-//       <ReviewListClientContainer codeId={codeId} reviews={reviews} />
-//     </>
-//   );
-// }
