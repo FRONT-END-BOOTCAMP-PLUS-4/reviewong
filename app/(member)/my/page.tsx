681efb8485267594856375538d2015b414083721
devGO <devbin20@gmail.com>
2025-05-28 09:57:17 +0900

    feat: 마이페이지 > UserSummaryContainer, UserReviewHeatmapContainer 추가 #32

--- app/(member)/my/page.tsx
+++ app/(member)/my/page.tsx
@@ -1,37 +1,11 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-// @ts-ignore
-import CalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
+import UserReviewHeatmapContainer from './containers/UserReviewHeatmapContainer';
+import UserSummaryContainer from './containers/UserSummaryContainer';
 
 export default function Mypage() {
-  const [heatmapData, setHeatmapData] = useState<{ date: string; count: number }[]>([]);
-
-  useEffect(() => {
-    const fetchHeatmapData = async () => {
-      try {
-        const res = await fetch('/api/member/my/review_counts');
-        if (!res.ok) {
-          throw new Error('Failed to fetch heatmap data');
-        }
-        const data = await res.json();
-        setHeatmapData(data.reviewHeatmap);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchHeatmapData();
-  }, []);
-
   return (
     <div>
-      <CalendarHeatmap
-        startDate={new Date('2025-01-01')}
-        endDate={new Date('2025-12-31')}
-        values={heatmapData}
-      />
+      <UserSummaryContainer />
+      <UserReviewHeatmapContainer />
     </div>
   );
 }
