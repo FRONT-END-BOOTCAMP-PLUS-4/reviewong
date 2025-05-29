932ad2850a4d27ef68be7e7ec6a8294054b0b9e6
DKULena <32173057@dankook.ac.kr>
2025-05-29 12:15:27 +0900

    refactor: API 요청 시 userId를 쿼리 파라미터로 전달하도록 수정 #29

--- app/containers/actions.ts
+++ app/containers/actions.ts
@@ -1,7 +1,16 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
 export async function fetchDailyChallenge() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/codes/daily`, {
-    cache: 'no-store',
-  });
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+  console.log('session', session);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/codes/daily${userId ? `?userId=${userId}` : ''}`,
+    {
+      cache: 'no-store',
+    }
+  );
 
   if (!res.ok) {
     if (res.status === 401) {
