5b78c3224c55dbce44cfeac13ced7ea51325ef0a
DKULena <32173057@dankook.ac.kr>
2025-05-29 17:21:17 +0900

    feat: 메인 페이지 최근 리뷰 컨테이너 생성 #29

---
+++ app/reviews/containers/LatestReviewContainer.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { ReviewView } from '@/domain/entities/ReviewView';
+import ReviewList from '../components/ReviewList';
+import { useSession } from 'next-auth/react';
+import ReviewFormContainer from './ReviewFormContainer';
+
+export default function LatestReviewListContainer({ codeId }: { codeId: number }) {
+  const { data: session } = useSession();
+  const {
+    data: reviews,
+    isLoading,
+    refetch,
+  } = useQuery<ReviewView[]>({
+    queryKey: ['latest-reviews', codeId],
+    queryFn: async () => {
+      const res = await fetch(`/api/codes/${codeId}/latest_reviews`, {
+        cache: 'no-store',
+      });
+      if (!res.ok) {
+        throw new Error('최근 리뷰 조회 실패');
+      }
+      return res.json();
+    },
+  });
+
+  if (isLoading || !reviews) {
+    return <div>리뷰 불러오는 중...</div>;
+  }
+
+  return (
+    <>
+      <h3 className="font-bold m-5">최근 리뷰</h3>
+
+      <ReviewFormContainer codeId={codeId} onDone={() => refetch()} />
+      <ReviewList
+        reviews={reviews}
+        codeId={codeId}
+        isAuthor={session?.user?.id ?? null}
+        showInteractions={false}
+      />
+    </>
+  );
+}
