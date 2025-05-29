1c7bd9bc45852defda21cfb94eb2f06f4588678c
DKULena <32173057@dankook.ac.kr>
2025-05-29 17:19:07 +0900

    feat: 메인 페이지 최근 리뷰 api 생성 #29

---
+++ app/api/codes/[id]/latest_reviews/route.ts
@@ -0,0 +1,22 @@
+// api/codes/[id]/latest_reviews/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { GetLatestReviewsUsecase } from '@/application/usecases/review/GetLatestReviewsUsecase';
+import { PrReviewRepository } from '@/infra/repositories/prisma/PrReviewRepository';
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const { id } = await params;
+    const reviewRepository = new PrReviewRepository();
+    const getLatestReviewsUsecase = new GetLatestReviewsUsecase(reviewRepository);
+    const result = await getLatestReviewsUsecase.execute(Number(id));
+
+    console.log('최신 리뷰 API result:', result);
+
+    if (!result.success) {
+      return NextResponse.json({ error: result.error }, { status: 404 });
+    }
+    return NextResponse.json(result.data);
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
