0087905e8b279805b5afd86342b7419c16970681
1juyoung <myjuyoung2@gmail.com>
2025-05-29 16:37:13 +0900

    remove: checkUserReview 삭제 #10

--- app/reviews/containers/ReviewListContainer.tsx
+++ app/reviews/containers/ReviewListContainer.tsx
@@ -1,26 +1,7 @@
 import { ReviewView } from '@/domain/entities/ReviewView';
 import ReviewListClientContainer from './ReviewListClientContainer';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
 
 export default async function ReviewListContainer({ codeId }: { codeId: number }) {
-  const session = await getServerSession(authOptions);
-  if (!session || !session.user.id) {
-    return <>로그인 하지 않은 사용자에게 보여줄 컴포넌트</>;
-  } else {
-    const checkUserReview = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/check_reviews`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${session.user.id}`,
-        },
-      }
-    );
-    if (!checkUserReview.ok) {
-      return <>리뷰 작성 여부를 확인하는 중 오류가 발생했습니다.</>;
-    }
-  }
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/reviews`,
     {
@@ -34,10 +15,5 @@ export default async function ReviewListContainer({ codeId }: { codeId: number }
 
   const reviews: ReviewView[] = await res.json();
 
-  return (
-    <>
-      <h3 className="font-bold m-5">리뷰 목록 ({reviews.length})</h3>
-      <ReviewListClientContainer codeId={codeId} initialReviews={reviews} />
-    </>
-  );
+  return <ReviewListClientContainer codeId={codeId} initialReviews={reviews} />;
 }
