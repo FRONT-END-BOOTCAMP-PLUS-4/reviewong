44540db33942ff02e6990427e8b186fa31f252d8
DKULena <32173057@dankook.ac.kr>
2025-05-30 17:25:29 +0900

    feat: 유저 랭킹 정보 조회 API 엔드포인트 구현 #29

---
+++ app/api/users/ranking/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { GetUserRankingUsecase } from '@/application/usecases/user/GetUserRankingUsecase';
+import { PrUserRepository } from '@/infra/repositories/prisma/PrUserRepository';
+
+const userRepository = new PrUserRepository();
+const getUserRankingUsecase = new GetUserRankingUsecase(userRepository);
+
+export async function GET(req: NextRequest) {
+  const count = 10;
+
+  const [likeRanking, reviewRanking] = await Promise.all([
+    getUserRankingUsecase.executeByLikes(count),
+    getUserRankingUsecase.executeByReviews(count),
+  ]);
+
+  return NextResponse.json({ likeRanking, reviewRanking });
+}
