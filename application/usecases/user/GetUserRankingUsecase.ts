b56e8fe30e0551e351372fc8e6708411624a35e6
DKULena <32173057@dankook.ac.kr>
2025-05-30 16:14:10 +0900

    feat: 유저 랭킹 조회 유즈케이스 생성 #29

---
+++ application/usecases/user/GetUserRankingUsecase.ts
@@ -0,0 +1,29 @@
+import { UserRepository } from '@/domain/repositories/UserRepository';
+import { GetUserRankingDto } from '@/application/usecases/user/dto/GetUserRankingDto';
+
+export class GetUserRankingUsecase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async executeByLikes(rankingCount: number): Promise<GetUserRankingDto[]> {
+    const users = await this.userRepository.getUserRankingByLikes(rankingCount);
+    return users.map((user) => ({
+      userId: user.id,
+      profileImageUrl: user.imageUrl,
+      nickname: user.nickname,
+      gradeId: user.gradeId,
+      likeCount: user.likeCount,
+      reviewCount: undefined,
+    }));
+  }
+  async executeByReviews(rankingCount: number): Promise<GetUserRankingDto[]> {
+    const users = await this.userRepository.getUserRankingByReviews(rankingCount);
+    return users.map((user) => ({
+      userId: user.id,
+      profileImageUrl: user.imageUrl,
+      nickname: user.nickname,
+      gradeId: user.gradeId,
+      likeCount: undefined,
+      reviewCount: user.reviewCount,
+    }));
+  }
+}
