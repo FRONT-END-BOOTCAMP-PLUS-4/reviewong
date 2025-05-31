48d9f282afc83b362917688deedd4bfe274f0e49
1juyoung <myjuyoung2@gmail.com>
2025-05-31 14:37:22 +0900

    feat: 사용자 리뷰 좋아요 반환을 위해 Header에 session.user.id 추가 #44

--- app/reviews/containers/ReviewListContainer.tsx
+++ app/reviews/containers/ReviewListContainer.tsx
@@ -1,11 +1,18 @@
 import { ReviewView } from '@/domain/entities/ReviewView';
 import ReviewListClientContainer from './ReviewListClientContainer';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 export default async function ReviewListContainer({ codeId }: { codeId: number }) {
+  const session = await getServerSession(authOptions);
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/reviews`,
     {
       cache: 'no-store',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: session ? `Bearer ${session.user.id}` : '',
+      },
     }
   );
 
