b6f5fe466a88cdb9907005c8544cb0e7f263bf82
devGO <devbin20@gmail.com>
2025-06-01 00:32:15 +0900

    feat: ActivityListSkeleton 생성 #42

---
+++ app/components/skeletons/ActivityListSkeleton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function ActivityListSkeleton() {
+  const dummyArray = Array.from({ length: 10 });
+
+  return (
+    <div className="space-y-4">
+      {dummyArray.map((_, index) => (
+        <div
+          key={index}
+          className="p-4 border rounded-2xl shadow-sm bg-white space-y-2 animate-pulse"
+        >
+          <div className="flex items-center space-x-2">
+            <Skeleton className="w-40 h-4" />
+          </div>
+          <Skeleton className="w-full h-16 rounded-md" />
+          <div className="flex justify-between items-center mt-2">
+            <Skeleton className="w-24 h-6.5" />
+            <Skeleton className="w-12 h-6.5" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
