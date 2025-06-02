8eada17f101a8f911b6a87c588e3cfd7c20a319e
devGO <devbin20@gmail.com>
2025-06-02 15:39:19 +0900

    fix: 불필요한 NextRequest 삭제

--- app/api/member/my/route.ts
+++ app/api/member/my/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { PrUserRepository } from '@/infra/repositories/prisma/PrUserRepository';
 import { GetUserSummaryUsecase } from '@/application/usecases/user/GetUserSummaryUsecase';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const session = await getServerSession(authOptions);
 
