643b36dc14a899653fb39702bb80892efa5d0abe
1juyoung <myjuyoung2@gmail.com>
2025-06-02 16:16:29 +0900

    fix: params 전달 방식 수정

--- app/codes/[id]/page.tsx
+++ app/codes/[id]/page.tsx
@@ -1,5 +1,9 @@
 import CodeReviewContainer from './CodeReviewContainer';
-export default async function CodeSnippetDetailPage({ params }: { params: { id: string } }) {
+export default async function CodeSnippetDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
 
   return <CodeReviewContainer id={id} />;
