acbcba29715ccfaa4176b37fa2b96d65e9cc40b4
1juyoung <myjuyoung2@gmail.com>
2025-06-04 15:24:09 +0900

    feat: 로직 개선 및 사용자와 작성자 id 확인 추가 #10

--- app/codes/[id]/CodeReviewContainer.tsx
+++ app/codes/[id]/CodeReviewContainer.tsx
@@ -9,42 +9,56 @@ import ReviewFormContainer from '@/app/reviews/containers/ReviewFormContainer';
 import CodeSnippetDetailContainer from '@/app/codes/[id]/CodeSnippetDetailContainer';
 import CodeDetailSkeleton from '@/app/components/skeletons/CodeDetailSkeleton';
 import ReviewListSkeleton from '@/app/components/skeletons/ReviewListSkeleton';
+import { notFound } from 'next/navigation';
 
 export default async function CodeReviewContainer({ id }: { id: string }) {
   const codeId = parseInt(id, 10);
-  let userReviewContent: string | undefined;
   const session = await getServerSession(authOptions);
 
-  if (!session || !session.user.id) {
-    return (
-      <>
-        <CodeSnippetDetailContainer id={id} />
-        <ReviewFormContainer codeId={codeId} />
-        <ReviewLock />
-      </>
-    );
+  let showReviewForm = false;
+  let userReviewContent: string | undefined;
+  let hasError = false;
+
+  if (!session?.user?.id) {
+    showReviewForm = true;
   } else {
-    const checkUserReview = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/check_reviews`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${session.user.id}`,
-        },
-      }
-    );
-    const { hasUserReviewed, data } = await checkUserReview.json();
-    if (hasUserReviewed === false) {
-      return (
-        <>
-          <CodeSnippetDetailContainer id={id} />
-          <ReviewFormContainer codeId={codeId} />
-          <ReviewLock />
-        </>
+    try {
+      const authoredCheck = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/codes/${codeId}/authored_reviews`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session.user.id}`,
+          },
+        }
       );
-    } else {
-      userReviewContent = data.reviewContent;
+      const { result } = await authoredCheck.json();
+
+      const checkUserReview = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/member/codes/${codeId}/check_reviews`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session.user.id}`,
+          },
+        }
+      );
+      const { hasUserReviewed, data } = await checkUserReview.json();
+      if (!result) {
+        if (!hasUserReviewed) {
+          showReviewForm = true;
+        } else {
+          userReviewContent = data.reviewContent;
+        }
+      }
+    } catch (e) {
+      console.error('Error fetching user review status:', e);
+      hasError = true;
     }
+  }
+
+  if (hasError) {
+    notFound();
   }
 
   return (
@@ -52,12 +66,20 @@ export default async function CodeReviewContainer({ id }: { id: string }) {
       <Suspense fallback={<CodeDetailSkeleton />}>
         <CodeSnippetDetailContainer
           id={id}
-          userId={session.user.id}
+          userId={session?.user?.id}
           userReviewContent={userReviewContent}
         />
       </Suspense>
+
       <Suspense fallback={<ReviewListSkeleton />}>
-        <ReviewListContainer codeId={codeId} />
+        {showReviewForm ? (
+          <>
+            <ReviewFormContainer codeId={codeId} />
+            <ReviewLock />
+          </>
+        ) : (
+          <ReviewListContainer codeId={codeId} />
+        )}
       </Suspense>
     </>
   );
